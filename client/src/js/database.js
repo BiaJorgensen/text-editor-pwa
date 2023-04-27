d7ce3819b692c85bc14f5f20e0682c0df9b36966
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-04-27 12:17:00 -0500

    Added logic to add new content to db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,19 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // Creating connection to the database and version
+  const jateDb = await openDB('jate', 1);
+  // Creating new transaction and establishing read and write privileges
+  const transaction = jateDb.transaction('jate', 'readwrite');
+  // Opening the jate object store
+  const store = transaction.objectStore('jate');
+  // Adding content to jate
+  const addContent = store.add( {text: content});
+  // Confirming content was added
+  const result = await addContent
+  console.log('New Content has been added', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
